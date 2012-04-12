4d5d96fb7391603242dd9ed35660b681b0641a79
Venkat Naidu <naiduv@bittorrent.com>
2012-04-11 20:38:29 -0700

    added some mouse functionality

--- test.js
+++ test.js
@@ -11,24 +11,47 @@ randfunc = function(){
 
 //update x and y coordinate with randfunc value
 Timer.update = function() {
+	if(isclicked){
+		x = mousex;
+		y = mousey;
+	}
+
 	x += randfunc();
 	y += randfunc();
 }
 
+function updatemouseloc(e) {
+	mousex = e.pageX;
+	mousey = e.pageY;
+}
+
+var isclicked = false;
+
+function setclicked(e) {
+	if(isclicked)
+		isclicked = false;
+	else
+		isclicked = true;
+}
+
 //when the page loads init your vars and get the canvas and context
 window.onload = function() {
-	x = 0;
-	y = 0;
+	x = 100;
+	y = 100;
+	mousex = 0;
+	mousey = 0;
 	c = document.getElementById("myCanvas");
 	ctx = c.getContext("2d");
 }
 
 //function to run on the timer!!
 Timer.run = function() {
-  Timer.update();
-  ctx.fillStyle="#1589FF";
-  ctx.fillRect(0,0,500,500);
-  ctx.drawImage(document.getElementById("flag"),x,y);
+	document.onmousemove = updatemouseloc;
+	document.onmousedown = setclicked;
+	Timer.update();
+	ctx.fillStyle="#1589FF";
+	ctx.fillRect(0,0,500,500);
+	ctx.drawImage(document.getElementById("flag"),x,y);
 };
 
 //timer interval is 10ms
