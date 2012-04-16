58c2f66dd63025c3de21011b56b6f7de290605e5
Venkat Naidu <naiduv@bittorrent.com>
2012-04-15 17:14:49 -0700

    added files

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 //this is your timer, does nothing right now
-Timer = function() {}
+Timer = function() {
+	this.stability = 10;
+}
 
 //randomly returns a +1 or -1
 randfunc = function(){
@@ -9,6 +11,13 @@ randfunc = function(){
 		return -1;
 }
 
+moveby = function(){
+	this.xi = 0;
+	this.yi = 0;
+}
+
+mb = new moveby();
+
 //update x and y coordinate with randfunc value
 Timer.update = function() {
 	if(isclicked){
@@ -16,8 +25,8 @@ Timer.update = function() {
 		y = mousey;
 	}
 
-	x += randfunc();
-	y += randfunc();
+	x += (randfunc()+(mb.xi));
+	y += (randfunc()+(mb.yi));
 }
 
 function updatemouseloc(e) {
@@ -28,31 +37,99 @@ function updatemouseloc(e) {
 var isclicked = false;
 
 function setclicked(e) {
+	if(e.pageX>500 || e.pageY>500)
+		return;
+
 	if(isclicked)
 		isclicked = false;
 	else
 		isclicked = true;
 }
 
+InitGui = function(gui){
+	newTimer = new Timer();
+	var controlstability = gui.add(newTimer,'stability',0,50);
+
+	// newMoveBy = new moveby();
+	var controlMoveByX = gui.add(mb,'xi',-5,5);
+	var controlMoveByY = gui.add(mb,'yi',-5,5);
+
+	controlstability.onChange(function(value){
+	clearInterval(Timer._intervalId);
+	Timer._intervalId = setInterval(Timer.run, value);
+	});
+
+	controlMoveByX.onChange(function(value){
+		mb.xi = value;
+	});
+
+	controlMoveByY.onChange(function(value){
+		mb.yi = value;
+	});
+
+}
+
 //when the page loads init your vars and get the canvas and context
 window.onload = function() {
-	x = 100;
-	y = 100;
+	x = 400;
+	y = 200;
 	mousex = 0;
 	mousey = 0;
+	
 	c = document.getElementById("myCanvas");
 	ctx = c.getContext("2d");
+
+	gui = new dat.GUI();
+
+	//initialize the gui
+	InitGui(gui);
+
+	Timer._intervalId = setInterval(Timer.run, Timer.stability);
+}
+
+updateGuiControls = function(gui) {
+	for (var i in gui.__controllers) {
+ 		gui.__controllers[i].updateDisplay();
+	}
+}
+
+window.onkeypress = function(e){
+	var evtobj=window.event? event : e;
+	var unicode=evtobj.charCode? evtobj.charCode : evtobj.keyCode;
+	var actualkey=String.fromCharCode(unicode);
+	
+	switch(actualkey) {
+		case "w":
+			if(mb.yi<5)
+				mb.yi++;
+			break;
+		case "s":
+			if(mb.yi>-5)
+				mb.yi--;
+			break;
+		case "a":
+			if(mb.xi<5)
+				mb.xi++;
+			break;
+		case "d":
+			if(mb.xi>-5)
+				mb.xi--;
+			break;
+		default:
+			return;
+	}
+	
+	updateGuiControls(gui);
 }
 
 //function to run on the timer!!
 Timer.run = function() {
+	ctx.fillStyle="#FFFFFF";
+	ctx.fillRect(x,y,50,50);
 	document.onmousemove = updatemouseloc;
 	document.onmousedown = setclicked;
 	Timer.update();
-	ctx.fillStyle="#1589FF";
-	ctx.fillRect(0,0,500,500);
 	ctx.drawImage(document.getElementById("flag"),x,y);
 };
 
 //timer interval is 10ms
-Timer._intervalId = setInterval(Timer.run, 10);
